84f5afce8dc7ee0d82189705f9424c5d570a1ed5
toan.nguyen3 <toan.nguyen3@mservice.com.vn>
2020-07-08 19:27:49 +0700

    update layout main and log in

---
+++ app/src/main/java/com/example/demostackexchange/activity/LeftMenuActivity.java
@@ -0,0 +1,19 @@
+package com.example.demostackexchange.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.demostackexchange.R;
+
+import java.util.Objects;
+
+public class LeftMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_left_menu);
+
+
+    }
+}
--- app/src/main/java/com/example/demostackexchange/activity/MainActivity.java
+++ app/src/main/java/com/example/demostackexchange/activity/MainActivity.java
@@ -57,6 +57,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        tvStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LeftMenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void init() {
